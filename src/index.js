3bb38f8792b776bd8a875b4c6fd43e62fccd8fdf
kevjose <kevin.jose@letsventure.com>
2016-12-23 12:52:37 +0530

    First component

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
+
+ReactDOM.render(
+  <div>
+    {
+      tracks.map((track, key) => {
+        return <div className="track" key={key}>{track.title}</div>;
+      })
+    }
+  </div>,
+  document.getElementById('app')
+);
